1fa5927aa6be5a1c111cc956c81b7cd18456ed5c
Gabriel <gvalentinsilva@gmail.com>
2024-03-31 13:17:01 -0300

    att

---
+++ funcoes_pratica.js
@@ -0,0 +1,15 @@
+function EscrevaNome(name){
+    console.log('Meu nome é ' + name);
+}
+
+EscrevaNome('Gabriel');
+
+function verificarIdade(idade) {
+    if (idade > 18) {
+        console.log('Maior');
+    } else {
+        console.log('Menor')
+    }
+}
+
+verificarIdade(25);
